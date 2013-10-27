9aa102af4a4b2d7fe8a65b1784d4103e3868f44e
yichao0319 <yichao0319@gmail.com>
2013-10-27 15:43:57 -0500

    detection and prediction using compressive sensing

---
+++ subtask_compressive_sensing/condor/aggregate_pred_output.pl
@@ -0,0 +1,231 @@
+#!/bin/perl
+
+##########################################
+## Author: Yi-Chao Chen
+## 2013.09.27 @ UT Austin
+##
+## - input:
+##
+## - output:
+##
+## - e.g.
+##
+##########################################
+
+use strict;
+
+#############
+# Debug
+#############
+my $DEBUG0 = 0;
+my $DEBUG1 = 1;
+my $DEBUG2 = 1; ## print progress
+my $DEBUG3 = 1; ## print output
+
+
+#############
+# Constants
+#############
+my $NUM_CURVE = 8;
+
+
+#############
+# Variables
+#############
+my $input_dir  = "/u/yichao/anomaly_compression/condor_data/subtask_compressive_sensing/condor/output";
+my $output_dir = "/u/yichao/anomaly_compression/condor_data/subtask_compressive_sensing/output";
+my $gnuplot_mother = "plot.pr";
+
+## data - TRACE - OPT_DECT - OPT_DELTA - BLOCK_SIZE - THRESH - [TP, TN, FP, TN, ...]
+my %data = ();
+## best - TRACE - [OPT_DECT | OPT_DELTA | BLOCK_SIZE] - [MSE | SETTING | FP | ...]
+my %best = ();
+
+
+#############
+# check input
+#############
+if(@ARGV != 0) {
+    print "wrong number of input: ".@ARGV."\n";
+    exit;
+}
+
+
+#############
+# Main starts
+#############
+my $func = "smrf_based_pred";
+open FH_OUT, "> $output_dir/$func.txt" or die $!;
+
+for my $file_name ("TM_Airport_period5_") {
+    
+    my $num_frames = 12;
+    my $width = 300;
+    my $height = 300;
+    if($file_name eq "TM_Manhattan_period5_") {
+        my $width = 500;
+        my $height = 500;
+    }
+
+    for my $loss_rate (0.001, 0.005, 0.01) {
+
+        for my $opt_swap_mat (0, 1, 2, 3) {
+            if(!(exists $best{TRACE}{"$file_name"}{OPT_SWAP_MAT}{$opt_swap_mat}{MSE})) {
+                $best{TRACE}{"$file_name"}{OPT_SWAP_MAT}{$opt_swap_mat}{MSE} = 0;
+            }
+
+            for my $group_size (4) {
+                if(!(exists $best{TRACE}{"$file_name"}{GROUP_SIZE}{$group_size}{MSE})) {
+                    $best{TRACE}{"$file_name"}{GROUP_SIZE}{$group_size}{MSE} = 0;
+                }
+                
+                for my $opt_type (0, 1) {
+                    if(!(exists $best{TRACE}{"$file_name"}{OPT_TYPE}{$opt_type}{MSE})) {
+                        $best{TRACE}{"$file_name"}{OPT_TYPE}{$opt_type}{MSE} = 0;
+                    }
+
+
+                    ## gnuplot - data
+                    open FH_OUT_2, "> $output_dir/$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.txt" or die $!;
+
+                    ## gnuplot - MSE
+                    my $cmd = "sed 's/FILENAME/$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate/g;s/FIGNAME/$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mse/g;s/X_RANGE_S//g;s/X_RANGE_E//g;s/Y_RANGE_S//g;s/Y_RANGE_E//g;s/X_LABEL/seed/g;s/Y_LABEL/MSE/g;s/DEGREE/-45/g;' $gnuplot_mother.mother.plot > tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mse.plot";
+                    `$cmd`;
+                    open FH_GNU, ">> tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mse.plot" or die $!;
+
+                    ## gnuplot - MAE
+                    my $cmd = "sed 's/FILENAME/$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate/g;s/FIGNAME/$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mae/g;s/X_RANGE_S//g;s/X_RANGE_E//g;s/Y_RANGE_S//g;s/Y_RANGE_E//g;s/X_LABEL/seed/g;s/Y_LABEL/MAE/g;s/DEGREE/-45/g;' $gnuplot_mother.mother.plot > tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mae.plot";
+                    `$cmd`;
+                    open FH_GNU, ">> tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mae.plot" or die $!;
+
+                    ## gnuplot - CC
+                    my $cmd = "sed 's/FILENAME/$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate/g;s/FIGNAME/$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.cc/g;s/X_RANGE_S//g;s/X_RANGE_E//g;s/Y_RANGE_S//g;s/Y_RANGE_E//g;s/X_LABEL/seed/g;s/Y_LABEL/CC/g;s/DEGREE/-45/g;' $gnuplot_mother.mother.plot > tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.cc.plot";
+                    `$cmd`;
+                    open FH_GNU, ">> tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.cc.plot" or die $!;
+
+
+                    my $first_seed = 1;
+                    for my $seed (1 .. 10) {
+                        print FH_OUT_2 "$seed, ";
+
+                        my $cnt = 0;
+                        for my $rank (1, 2, 3, 5, 7, 10, 20, 30, 50) {
+                            if(!(exists $best{TRACE}{"$file_name"}{RANK}{$rank}{MSE})) {
+                                $best{TRACE}{"$file_name"}{RANK}{$rank}{MSE} = 0;
+                            }
+
+                            
+
+                            my $this_file_name = "$input_dir/$func.$file_name.$num_frames.$width.$height.$group_size.$rank.$opt_swap_mat.$opt_type.$loss_rate.$seed.txt";
+
+                            print "$this_file_name\n";
+                            
+                            open FH, $this_file_name or die $!;
+                            while(<FH>) {
+                                my @ret = split(/, /, $_);
+                                my $mse = $ret[0] + 0;
+                                my $mae = $ret[1] + 0;
+                                my $cc = $ret[2] + 0;
+                                
+                                $data{TRACE}{"$file_name"}{OPT_SWAP_MAT}{$opt_swap_mat}{GROUP_SIZE}{$group_size}{RANK}{$rank}{OPT_TYPE}{$opt_type}{LOSS_RATE}{$loss_rate}{SEED}{$seed}{MSE} = $mse;
+                                $data{TRACE}{"$file_name"}{OPT_SWAP_MAT}{$opt_swap_mat}{GROUP_SIZE}{$group_size}{RANK}{$rank}{OPT_TYPE}{$opt_type}{LOSS_RATE}{$loss_rate}{SEED}{$seed}{MAE} = $mae;
+                                $data{TRACE}{"$file_name"}{OPT_SWAP_MAT}{$opt_swap_mat}{GROUP_SIZE}{$group_size}{RANK}{$rank}{OPT_TYPE}{$opt_type}{LOSS_RATE}{$loss_rate}{SEED}{$seed}{CC} = $cc;
+
+                                my $buf = "$file_name, $num_frames, $width, $height, $opt_swap_mat, $group_size, $rank, $opt_type, $loss_rate, $seed, $mse, $mae, $cc\n";
+                                print $buf;
+                                print FH_OUT $buf;
+
+
+                                ####################
+                                if($mse < $best{TRACE}{"$file_name"}{OPT_TYPE}{$opt_type}{MSE}) {
+                                    $best{TRACE}{"$file_name"}{OPT_TYPE}{$opt_type}{MSE} = $mse;
+                                }
+                                if($mse < $best{TRACE}{"$file_name"}{GROUP_SIZE}{$group_size}{MSE}) {
+                                    $best{TRACE}{"$file_name"}{GROUP_SIZE}{$group_size}{MSE} = $mse;
+                                }
+                                if($mse < $best{TRACE}{"$file_name"}{RANK}{$rank}{MSE}) {
+                                    $best{TRACE}{"$file_name"}{RANK}{$rank}{MSE} = $mse;
+                                }
+                                if($mse < $best{TRACE}{"$file_name"}{OPT_SWAP_MAT}{$opt_swap_mat}{MSE}) {
+                                    $best{TRACE}{"$file_name"}{OPT_SWAP_MAT}{$opt_swap_mat}{MSE} = $mse;
+                                }
+
+
+                                ####################
+                                ## gnuplot - data
+                                print FH_OUT_2 "$rank, $mse, $mae, $cc, ";
+                                
+                                
+                                if($first_seed == 1 and $cnt < $NUM_CURVE){
+                                    ## gnuplot - MSE
+                                    my $ind = 1 + $cnt * 4 + 2;
+                                    my $ls_cnt = $cnt % 8 + 1;
+                                    print FH_GNU_MSE "," if($cnt != 0);
+                                    print FH_GNU_MSE " \\\n";
+                                    print FH_GNU_MSE "data_dir.file_name.\".txt\" using 1:$ind with linespoints ls $ls_cnt title '{/Helvetica=28 rank=$rank}'";
+
+                                    ## gnuplot - MAE
+                                    $ind = 1 + $cnt * 4 + 3;
+                                    print FH_GNU_MAE "," if($cnt != 0);
+                                    print FH_GNU_MAE " \\\n";
+                                    print FH_GNU_MAE "data_dir.file_name.\".txt\" using 1:$ind with linespoints ls $ls_cnt title '{/Helvetica=28 rank=$rank}'";
+
+                                    ## gnuplot - CC
+                                    $ind = 1 + $cnt * 4 + 4;
+                                    print FH_GNU_CC "," if($cnt != 0);
+                                    print FH_GNU_CC " \\\n";
+                                    print FH_GNU_CC "data_dir.file_name.\".txt\" using 1:$ind with linespoints ls $ls_cnt title '{/Helvetica=28 rank=$rank}'";
+                                    
+                                    $cnt ++;
+                                    
+                                }
+                            }
+                            close FH;
+                        }
+                        $first_seed = 0;
+                        print FH_OUT_2 "\n";
+                    }
+                    close FH_OUT_2;
+                    close FH_GNU_MSE;
+                    close FH_GNU_MAE;
+                    close FH_GNU_CC;
+
+                    ## gnuplot
+                    $cmd = "gnuplot tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mse.plot";
+                    `$cmd`;
+
+                    $cmd = "gnuplot tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.mae.plot";
+                    `$cmd`;
+
+                    $cmd = "gnuplot tmp.$gnuplot_mother.$func.$file_name.$group_size.$opt_swap_mat.$opt_type.$loss_rate.cc.plot";
+                    `$cmd`;
+                }
+            }
+        }
+    }
+}
+close FH_OUT;
+
+
+
+#############
+# Statistics
+#############
+## best - TRACE - [OPT_DECT | OPT_DELTA | BLOCK_SIZE] - [MSE | SETTING | FP | ...]
+foreach my $trace (sort {$a cmp $b} (keys %{ $best{TRACE} })) {
+    foreach my $group_size (sort {$a <=> $b} (keys %{ $best{TRACE}{$trace}{GROUP_SIZE} })) {
+        print "$trace, group_size=$group_size, ".$best{TRACE}{$trace}{GROUP_SIZE}{$group_size}{MSE}."\n";
+    }
+
+    foreach my $rank (sort {$a <=> $b} (keys %{ $best{TRACE}{$trace}{RANK} })) {
+        print "$trace, rank=$rank, ".$best{TRACE}{$trace}{RANK}{$rank}{MSE}."\n";
+    }
+
+    foreach my $opt_type (sort {$a <=> $b} (keys %{ $best{TRACE}{$trace}{OPT_TYPE} })) {
+        print "$trace, opt_type=$opt_type, ".$best{TRACE}{$trace}{OPT_TYPE}{$opt_type}{MSE}."\n";
+    }
+
+    foreach my $opt_swap_mat (sort {$a <=> $b} (keys %{ $best{TRACE}{$trace}{OPT_SWAP_MAT} })) {
+        print "$trace, opt_swap_mat=$opt_swap_mat, ".$best{TRACE}{$trace}{OPT_SWAP_MAT}{$opt_swap_mat}{MSE}."\n";
+    }
+}
---
+++ subtask_compressive_sensing/condor/smrf_based_pred.m
@@ -0,0 +1,305 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Yi-Chao Chen
+%% 2013.10.26 @ UT Austin
+%%
+%% - Input:
+%%   @option_swap_mat: determine how to arrange rows and columns of TM
+%%      0: original matrix
+%%      1: randomize raw and col
+%%      2: geo
+%%      3: correlated coefficient
+%%   @option_type: determine the type of estimation
+%%      0: baseline
+%%      1: SRMF
+%%
+%% - Output:
+%%
+%% e.g. 
+%%     [mse, mae, cc] = smrf_based_pred('TM_Airport_period5_', 12, 300, 300, 4, 5, 0, 1, 0.001, 1)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [mse, mae, cc] = smrf_based_pred(filename, num_frames, width, height, group_size, r, option_swap_mat, option_type, loss_rate, seed)
+    addpath('/u/yichao/anomaly_compression/utils/mirt_dctn');
+    addpath('/u/yichao/anomaly_compression/utils/compressive_sensing');
+    addpath('/u/yichao/anomaly_compression/utils');
+
+
+    %% --------------------
+    %% DEBUG
+    %% --------------------
+    DEBUG0 = 0;
+    DEBUG1 = 1;
+    DEBUG2 = 1;
+    DEBUG3 = 0; %% block index check
+
+    if width ~= height
+        fprintf('width should be equal to height: %d, %d\n', width, height);
+        return;
+    end
+
+
+    %% --------------------
+    %% Constant
+    %% --------------------
+    
+
+    %% --------------------
+    %% Variable
+    %% --------------------
+    input_TM_dir   = '/u/yichao/anomaly_compression/condor_data/subtask_process_4sq/TM/';
+    input_errs_dir = '/u/yichao/anomaly_compression/condor_data/subtask_inject_error/errs/';
+    input_4sq_dir  = '/u/yichao/anomaly_compression/condor_data/subtask_process_4sq/TM/';
+    
+
+
+    %% --------------------
+    %% Main starts
+    %% --------------------
+    rand('seed', seed);
+    num_groups = ceil(num_frames / group_size);
+
+
+    %% --------------------
+    %% Read data matrix
+    %% --------------------
+    if DEBUG2, fprintf('read data matrix\n'); end
+
+    data = zeros(width, height, num_frames);
+    for frame = [0:num_frames-1]
+        if DEBUG0, fprintf('  frame %d\n', frame); end
+
+        %% load data matrix
+        this_matrix_file = [input_TM_dir filename int2str(frame) '.txt'];
+        if DEBUG0, fprintf('    file = %s\n', this_matrix_file); end
+        
+        tmp = load(this_matrix_file);
+        data(:,:,frame+1) = tmp(1:width, 1:height);
+    end
+    sx = size(data(:,:,1));
+    nx = prod(sx);
+
+
+    %% --------------------
+    %% drop elements
+    %% --------------------
+    if DEBUG2, fprintf('drop elements\n'); end
+
+    M = ones(size(data));
+    num_missing = ceil(nx * loss_rate);
+    for f = [1:num_frames]
+        if DEBUG0, fprintf('  frame %d\n', f); end
+
+        ind = randperm(nx);
+        tmp = M(:,:,f);
+        tmp(ind(1:num_missing)) = 0;
+        M(:,:,f) = tmp;
+    end
+
+
+    %% --------------------
+    %% swap matrix row and column
+    %% 0: original matrix
+    %% 1: randomize raw and col
+    %% 2: geo
+    %% 3: correlated coefficient
+    %% --------------------
+    if DEBUG2, fprintf('swap matrix row and column\n'); end
+
+    if option_swap_mat == 0
+        %% 0: original matrix
+        mapping = [1:width];
+    elseif option_swap_mat == 1
+        %% 1: randomize raw and col
+        mapping = randperm(width);
+    elseif option_swap_mat == 2
+        %% 2: geo
+        [location, mass] = get_venue_info([input_4sq_dir filename], '4sq', width, height);
+        if DEBUG0
+            fprintf('  size of location: %d, %d\n', size(location));
+            fprintf('  size of mass: %d, %d\n', size(mass));
+        end
+        
+        mapping = sort_by_lat_lng(location, width, height);
+
+    elseif option_swap_mat == 3
+        %% 3: correlated coefficient
+        tmp = reshape(data, width, []);
+        if DEBUG1
+            fprintf('  size of the whole matrix: %d, %d\n', size(tmp));
+        end
+        
+        coef = corrcoef(tmp');
+        mapping = sort_by_coef(coef, width, height);
+    end
+
+    %% update the data matrix and ground truth according to the mapping
+    for f = [1:num_frames]
+        data(:,:,f) = map_matrix(data(:,:,f), mapping);
+        M(:,:,f)    = map_matrix(M(:,:,f), mapping);
+    end
+
+    if DEBUG1, fprintf('  size of data matrix: %d, %d, %d\n', size(data)); end
+    
+
+    compared_data = data;
+    compared_data(~M) = 0;
+
+
+    %% --------------------
+    %% apply SMRF to each Group of Pictures (GoP)
+    %% --------------------
+    for gop = 1:num_groups
+        gop_s = (gop - 1) * group_size + 1;
+        gop_e = min(num_frames, gop * group_size);
+
+        if DEBUG1 == 0, fprintf('gop %d: frame %d-%d\n', gop, gop_s, gop_e); end
+
+        this_group   = data(:, :, gop_s:gop_e);
+        this_group_M = M(:, :, gop_s:gop_e);
+
+        
+
+        %% --------------------
+        %  Compressive Sensing
+        %% --------------------
+        this_rank = r; %min(r, rank(this_group));
+        lambda = 0.01;
+
+        % meanX2 = mean(this_group(:).^2);
+        % meanX = mean(this_group(:));
+        sx = size(this_group);
+        nx = prod(sx);
+        n  = length(sx);
+
+        [A, b] = XM2Ab(this_group, this_group_M);
+
+        if option_type == 0
+            %% baseline
+            est_group = EstimateBaseline(A, b, sx);
+        elseif option_type == 1
+            %% SRMF
+            config = ConfigSRTF(A, b, this_group, this_group_M, sx, this_rank, this_rank, lambda, true);
+            [u4, v4, w4] = SRTF(this_group, this_rank, this_group_M, config, 1000, 100, 50);
+
+            est_group = tensorprod(u4, v4, w4);
+            est_group = max(0, est_group);
+        end
+        
+        compared_data(:, :, gop_s:gop_e) = est_group;
+    end
+
+
+    meanX2 = mean(data(:).^2);
+    meanX = mean(data(:));
+    mse = mean(( data(~M) - max(0,compared_data(~M)) ).^2) / meanX2;
+    mae = mean(abs((data(~M) - max(0,compared_data(~M))))) / meanX;
+    cc  = corrcoef(data(~M),max(0,compared_data(~M)));
+    cc  = cc(1,2);
+end
+
+
+
+
+%% -------------------------------------
+%% map_matrix: swap row and columns according to "mapping"
+%% @input mapping: 
+%%    a vector to map venues to the other
+%%    e.g. [4, 3, 1, 2] means mapping 1->4, 2->3, 3->1, 4->2
+%%
+function [new_mat] = map_matrix(mat, mapping)
+    new_mat = zeros(size(mat));
+    new_mat(mapping, :) = mat;
+    tmp = new_mat;
+    new_mat(:, mapping) = tmp;
+end
+
+
+%% find_ind: function description
+function [map_ind] = find_mapping_ind(ind, width, height, mapping)
+    y = mod(ind-1, height) + 1;
+    x = floor((ind-1)/height) + 1;
+
+    x2 = mapping(x);
+    y2 = mapping(y);
+    map_ind = (x2 - 1) * height + y2;
+end
+
+
+%% -------------------------------------
+%% sort_by_lat_lng
+%% @input location: 
+%%    a Nx2 matrix to represent the (lat, lng) of N venues
+%%
+function [mapping] = sort_by_lat_lng(location, width, height)
+    mapping = ones(1, width);
+    tmp = 2:width;
+    src = 1;
+    src_ind = 2;
+    while length(tmp) > 0
+        min_dist = -1;
+        min_dist_dst = 0;
+        min_dist_ind = 0;
+
+        ind = 0;
+        for dst = tmp
+            ind = ind + 1;
+            dist = pos2dist(location(src,1), location(src,2), location(dst,1), location(dst,2), 2);
+
+            if (min_dist == -1) | (min_dist > dist) 
+                min_dist = dist;
+                min_dist_dst = dst;
+                min_dist_ind = ind;
+            end
+        end
+
+        if tmp(min_dist_ind) ~= min_dist_dst
+            fprintf('min dist dst does not match: %d, %d\n', tmp(min_dist_ind), min_dist_dst);
+            return;
+        end
+
+        mapping(src_ind) = min_dist_dst;
+        src = min_dist_dst;
+        src_ind = src_ind + 1;
+        tmp(min_dist_ind) = [];
+    end
+end
+
+
+%% -------------------------------------
+%% sort_by_coef
+%% @input coef: 
+%%    a NxN matrix to represent the correlation coefficient of N venues
+%%
+function [mapping] = sort_by_coef(coef, width, height)
+    mapping = ones(1, width);
+    tmp = 2:width;
+    src = 1;
+    src_ind = 2;
+    while length(tmp) > 0
+        max_coef = -1;
+        max_coef_dst = 0;
+        max_coef_ind = 0;
+
+        ind = 0;
+        for dst = tmp
+            ind = ind + 1;
+            this_coef = coef(src, dst);
+
+            if (max_coef == -1) | (this_coef > max_coef) 
+                max_coef = this_coef;
+                max_coef_dst = dst;
+                max_coef_ind = ind;
+            end
+        end
+
+        if tmp(max_coef_ind) ~= max_coef_dst
+            fprintf('max coef dst does not match: %d, %d\n', tmp(max_coef_ind), max_coef_dst);
+            return;
+        end
+
+        mapping(src_ind) = max_coef_dst;
+        src = max_coef_dst;
+        src_ind = src_ind + 1;
+        tmp(max_coef_ind) = [];
+    end
+end
