83def3a27df49beab2c9ffd50128a8f4a5f3580e
yichao0319 <yichao0319@gmail.com>
2013-10-30 15:00:40 -0500

    group and sort by [lat, lng]

--- subtask_parse_sjtu_wifi/sort_ips.pl
+++ subtask_parse_sjtu_wifi/sort_ips.pl
@@ -35,6 +35,7 @@ use Getopt::Long;
 use lib "../utils";
 
 use IPTool;
+use MyUtil;
 
 #############
 # Debug
@@ -53,11 +54,19 @@ my $DEBUG3 = 1; ## print output
 #############
 # Variables
 #############
-my $input_dir   = "../processed_data/subtask_parse_sjtu_wifi/text";
-my $table_dir   = "../processed_data/subtask_parse_sjtu_wifi/ip_info";
-my $output_dir = "";
+my $input_trace_dir  = "../processed_data/subtask_parse_sjtu_wifi/text";
+my $all_ips_dir      = "../processed_data/subtask_parse_sjtu_wifi/ip_info";
+my $table_dir        = "../processed_data/subtask_parse_sjtu_wifi/ip_info";
+my $invalid_dir      = "../processed_data/subtask_parse_sjtu_wifi/ip_info";
+my $account_dir      = "../data/sjtu_wifi/RADIUS";
+my $ap_dir           = "../data/sjtu_wifi";
+my $output_dir       = "../processed_data/subtask_parse_sjtu_wifi/sort_ips";
 
 my $table_file   = "ip_geo_as_table.txt";
+my $invalid_file = "ip_geo_as_invalid.txt";
+my $all_ips_file = "all_ips.txt";
+my $ap_file      = "AP_Location.csv";
+
 
 my $sjtu;
 my $other;
@@ -65,6 +74,15 @@ my $res;
 my $mask;
 
 my %ip_table_info = ();
+my %invalid_info = ();
+my %account_info = ();
+my %ap_info = ();
+my %ip_info = ();
+my %group_info = ();
+
+
+my $cnt_sjtu = 0;
+my $cnt_missing_sjtu = 0;
 
 
 #############
@@ -89,6 +107,22 @@ if($sjtu eq "gps" or $other eq "gps") {
 #############
 
 #############
+## Read RADIUS account data
+#############
+print "Read RADIUS account data: $account_dir\n" if($DEBUG2);
+%account_info = IPTool::read_account_info($account_dir);
+print "  size=".scalar(keys %{ $account_info{USER_IP} })."\n" if($DEBUG1);
+
+
+#############
+## Read AP location
+#############
+print "Read AP location: $ap_dir/$ap_file\n" if($DEBUG2);
+%ap_info = IPTool::read_ap_info("$ap_dir/$ap_file");
+print "  size=".scalar(keys %{ $ap_info{AP_MAC} })."\n" if($DEBUG1);
+
+
+#############
 # read IP GEO/AS Info
 #############
 print "read IP GEO/AS Info\n" if($DEBUG2);
@@ -96,21 +130,147 @@ print "read IP GEO/AS Info\n" if($DEBUG2);
 
 
 #############
-## search all traces
+# read invalid IPs
 #############
-print "search all traces\n" if($DEBUG2);
+print "read invalid IPs\n" if($DEBUG2);
 
-opendir(DIR, "$input_dir") or die $!;
-while (my $file = readdir(DIR)) {
-    next if($file =~ /^\.+/);  ## don't show "." and ".."
-    next if(-d "$input_dir/$file");  ## don't show directories
-    next unless($file =~ /gz$/);
+open FH, "$invalid_dir/$invalid_file" or die $!;
+while(<FH>) {
+    chomp;
+    
+    my $ip = $_;
+    next unless($ip =~ /\d+\.\d+\.\d+\.\d+/);
 
-    print "$file\n";
+    print "- ".$ip."\n" if($DEBUG0);
+    $invalid_info{IP}{$ip} = 1;
+}
+close FH;
+
+
+#############
+## get all ips
+#############
+print "get all ips\n" if($DEBUG2);
+
+open FH, "$all_ips_dir/$all_ips_file" or die $!;
+while(<FH>) {
+    chomp;
+    my $ip = $_;
+    next unless($ip =~ /\d+\.\d+\.\d+\.\d+/);
     
 
-    #############
-    ## open file
-    #############
+    ###################
+    ## skip invalid IPs and unknown IPs
+    ###################
+    next if(exists $invalid_info{IP}{$ip});
+    next unless(exists $ip_table_info{IP}{$ip});
+    die "should not have duplicate IP\n" if(exists $ip_info{IP}{$ip});
+
+    $ip_info{IP}{$ip} = $ip_table_info{IP}{$ip};
+
+
+    ###################
+    ## SJTU machines
+    ###################
+    if(exists $account_info{USER_IP}{$ip}) {
+        $cnt_sjtu ++;
+
+        print "  $ip\n" if($DEBUG0);
+        if($sjtu eq "gps") {
+            my $lat = $ip_info{IP}{$ip}{LAT} + 0;
+            my $lng = $ip_info{IP}{$ip}{LNG} + 0;
+
+            my $group = "".(int($lat / $res)).";".(int($lng / $res));
+            print "($lat, $lng) = $group\n" if($DEBUG0);
+
+            $group_info{START_GRP} = $group unless(exists $group_info{START_GRP});
+            $group_info{GROUP}{$group}{LAT} = $lat;
+            $group_info{GROUP}{$group}{LNG} = $lng;
+        }
+    }
+    else {
+
+        ## this IP is supposed to be SJTU machine but cannot find it in Account info
+        if($ip =~ /111\.186\.\d+\.\d+/) {
+            $cnt_missing_sjtu ++;
+        }
+
+        if($other eq "gps") {
+            my $lat = $ip_info{IP}{$ip}{LAT} + 0;
+            my $lng = $ip_info{IP}{$ip}{LNG} + 0;
+
+            my $group = "".(int($lat / $res)).";".(int($lng / $res));
+            print "($lat, $lng) = $group\n" if($DEBUG0);
+
+            $group_info{GROUP}{$group}{LAT} = $lat;
+            $group_info{GROUP}{$group}{LNG} = $lng;
+        }
+    }
 }
-closedir(DIR);
+close FH;
+
+
+print "# sjtu devices: $cnt_sjtu\n";
+print "# sjtu missing devices: $cnt_missing_sjtu\n";
+print "# group: ".scalar(keys %{ $group_info{GROUP} } )."\n";
+
+
+################
+## start to sort
+################
+my $cur_ind = 1;
+if($sjtu eq "gps") {
+    my @tmp_grps = keys %{ $group_info{GROUP} };
+
+    my $cur_grp = $group_info{START_GRP};
+    my $cur_lat = $group_info{GROUP}{$cur_grp}{LAT};
+    my $cur_lng = $group_info{GROUP}{$cur_grp}{LNG};
+    $group_info{GROUP}{$cur_grp}{IND} = $cur_ind;
+
+    @tmp_grps = grep { $_ != $cur_grp } @tmp_grps;
+
+    while(scalar(@tmp_grps) > 0) {
+        
+        my $min_dist = -1;
+        my $min_grp;
+        foreach my $this_grp (@tmp_grps) {
+            my $this_lat = $group_info{GROUP}{$this_grp}{LAT};
+            my $this_lng = $group_info{GROUP}{$this_grp}{LNG};
+
+            my $this_dist = MyUtil::pos2dist($cur_lat, $cur_lng, $this_lat, $this_lng);
+            if($this_dist < $min_dist or $min_dist == -1) {
+                $min_dist = $this_dist;
+                $min_grp = $this_grp;
+            }
+        }
+
+        @tmp_grps = grep { $_ != $min_grp } @tmp_grps;
+        $cur_grp = $min_grp;
+        $cur_lat = $group_info{GROUP}{$min_grp}{LAT};
+        $cur_lng = $group_info{GROUP}{$min_grp}{LNG};
+
+        $cur_ind ++;
+        $group_info{GROUP}{$cur_grp}{IND} = $cur_ind;
+    }
+}
+
+
+
+################
+## output ip => index (start from 1)
+################
+if($sjtu eq "gps") {
+    open FH, "> $output_dir/sort_ips.$sjtu.$other.$res.txt" or die $!;
+    foreach my $ip (sort {$a cmp $b} (keys %{ $ip_info{IP} })) {
+        my $lat = $ip_info{IP}{$ip}{LAT} + 0;
+        my $lng = $ip_info{IP}{$ip}{LNG} + 0;
+
+        my $group = "".(int($lat / $res)).";".(int($lng / $res));
+        my $index = $group_info{GROUP}{$group}{IND};
+
+        print FH "$ip, $index\n";
+    }
+    close FH;
+}
+
+
