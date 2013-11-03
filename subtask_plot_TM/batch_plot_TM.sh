1e37615a79f36f71539af91b9f4d77f1094283a7
yichao0319 <yichao0319@gmail.com>
2013-11-02 22:06:50 -0500

    store the commands for all TMs

--- subtask_plot_TM/batch_plot_TM.sh
+++ subtask_plot_TM/batch_plot_TM.sh
@@ -2,4 +2,4 @@
 
 perl plot_TM.pl ../processed_data/subtask_parse_huawei_3g/signaling_tm/tm.select_matrix_for_id-Assignment.txt.60 60 900
 perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.country.txt.3600 600 3000
-perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.gps.4.txt.3600 800
+perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.gps.4.txt.3600 800 2000
