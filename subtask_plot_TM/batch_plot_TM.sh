f22d360432a26c85eb70320dd5e2b258e11532fc
yichao0319 <yichao0319@gmail.com>
2013-11-07 20:55:13 -0600

    plot new TM

--- subtask_plot_TM/batch_plot_TM.sh
+++ subtask_plot_TM/batch_plot_TM.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-perl plot_TM.pl ../processed_data/subtask_parse_huawei_3g/signaling_tm/tm.select_matrix_for_id-Assignment.txt.60 60 900
-perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.country.txt.3600 600 3000
-perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.gps.4.txt.3600 800 2000
+# perl plot_TM.pl ../processed_data/subtask_parse_huawei_3g/signaling_tm/tm.select_matrix_for_id-Assignment.txt.60 60 900
+perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.country.txt.3600 900 3000
+perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.gps.1.sub_CN.txt.3600 900 3000  
+perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.gps.5.txt.3600 900 3000
+perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.bgp.8.txt.3600 900 3000
+perl plot_TM.pl ../processed_data/subtask_parse_sjtu_wifi/tm/tm.sort_ips.ap.bgp.10.sub_CN.txt.3600 900 3000  
