032d63c29fdbf760f6c538ab2f00f4d318052c43
yichao0319 <yichao0319@gmail.com>
2013-11-25 18:20:57 -0600

    add an option to do compression; calculate space

--- subtask_pca/condor/pca_based_pred.mother.sh
+++ subtask_pca/condor/pca_based_pred.mother.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-matlab -r "[mse, mae, cc] = pca_based_pred('INPUT_DIR', 'FILENAME', NUM_FRAMES, WIDTH, HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT, RANK, OPT_DECT, OPT_SWAP_MAT, LOSS_RATE, SEED); fh = fopen(['/u/yichao/anomaly_compression/condor_data/subtask_pca/condor/output/pca_based_pred.FILENAME.NUM_FRAMES.WIDTH.HEIGHT.BLOCK_WIDTH.BLOCK_HEIGHT.RANK.OPT_DECT.OPT_SWAP_MAT.LOSS_RATE.SEED.txt'], 'w'); fprintf(fh, '%f, %f, %f\n', mse, mae, cc); fclose(fh); exit;"
+/lusr/bin/matlab -r "[mse, mae, cc, ratio] = pca_based_pred('INPUT_DIR', 'FILENAME', NUM_FRAMES, WIDTH, HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT, RANK, OPT_SWAP_MAT, LOSS_RATE, SEED); fh = fopen(['/u/yichao/anomaly_compression/condor_data/subtask_pca/condor/output/pca_based_pred.FILENAME.NUM_FRAMES.WIDTH.HEIGHT.BLOCK_WIDTH.BLOCK_HEIGHT.RANK.OPT_SWAP_MAT.LOSS_RATE.SEED.txt'], 'w'); fprintf(fh, '%f, %f, %f, %f\n', mse, mae, cc, ratio); fclose(fh); exit;"
+
+# bash run_pca_based_pred.sh /v/filer4b/software/matlab-2011a 'INPUT_DIR', 'FILENAME' NUM_FRAMES WIDTH HEIGHT BLOCK_WIDTH BLOCK_HEIGHT RANK OPT_SWAP_MAT LOSS_RATE SEED
