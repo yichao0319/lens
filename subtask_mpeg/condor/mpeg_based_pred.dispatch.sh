a58b9462c8f5d229126366cef0d108d0688bc689
yichao0319 <yichao0319@gmail.com>
2013-11-25 18:14:13 -0600

    add an option to do compression; calculate space

--- subtask_mpeg/condor/mpeg_based_pred.dispatch.sh
+++ subtask_mpeg/condor/mpeg_based_pred.dispatch.sh
@@ -5,73 +5,87 @@ func="mpeg_based_pred"
 
 # for filename in "TM_Airport_period5_" "tm.sort_ips.ap.gps.5.txt.3600." "tm.select_matrix_for_id-Assignment.txt.60." "tm.sort_ips.ap.country.txt.3600." "tm.sort_ips.ap.bgp.8.txt.3600." "tm.sort_ips.ap.bgp.10.sub_CN.txt.3600." ; do
 # for filename in "tm.sort_ips.ap.gps.1.sub_CN.txt.3600."; do
-for filename in "tm.sort_ips.ap.country.txt.3600."  "tm.sort_ips.ap.bgp.8.txt.3600." "tm.sort_ips.ap.bgp.10.sub_CN.txt.3600."; do
+# for filename in "tm.sort_ips.ap.country.txt.3600."  "tm.sort_ips.ap.bgp.8.txt.3600." "tm.sort_ips.ap.bgp.10.sub_CN.txt.3600."; do
 
-    #############
-    if [[ ${filename} == "TM_Airport_period5_" ]]; then
-        input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_process_4sq\/TM\/"
-        num_frames=12
-        width=300
-        height=300
+# for filename in "tm_download.sort_ips.ap.bgp.sub_CN.txt.3600.top400." "tm_upload.sort_ips.ap.bgp.sub_CN.txt.3600.top400." "tm_download.sort_ips.ap.country.txt.3600.top400." "tm_upload.sort_ips.ap.country.txt.3600.top400." "tm_download.sort_ips.ap.gps.1.sub_CN.txt.3600.top400." "tm_upload.sort_ips.ap.gps.1.sub_CN.txt.3600.top400." "tm_download.sort_ips.ap.gps.5.txt.3600.top400." "tm_upload.sort_ips.ap.gps.5.txt.3600.top400."; do
+for filename in "tm_download.sort_ips.ap.bgp.sub_CN.txt.3600.top400."; do
 
-        block_sizes=(30)
-    fi
-    #############
-    if [[ ${filename} == "tm.select_matrix_for_id-Assignment.txt.60." ]]; then
-        input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_huawei_3g\/signaling_tm\/"
-        num_frames=12
-        width=28
-        height=28
 
-        block_sizes=(10 14 28)
-    fi
-    #############
-    if [[ ${filename} == "tm.sort_ips.ap.country.txt.3600." ]]; then
+    # #############
+    # if [[ ${filename} == "TM_Airport_period5_" ]]; then
+    #     input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_process_4sq\/TM\/"
+    #     num_frames=12
+    #     width=300
+    #     height=300
+
+    #     block_sizes=(30)
+    # fi
+    # #############
+    # if [[ ${filename} == "tm.select_matrix_for_id-Assignment.txt.60." ]]; then
+    #     input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_huawei_3g\/signaling_tm\/"
+    #     num_frames=12
+    #     width=28
+    #     height=28
+
+    #     block_sizes=(10 14 28)
+    # fi
+    # #############
+    # if [[ ${filename} == "tm.sort_ips.ap.country.txt.3600." ]]; then
+    #     input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
+    #     num_frames=7
+    #     width=400
+    #     height=400
+
+    #     block_sizes=(40 100 200)
+    # fi
+    # if [[ ${filename} == "tm.sort_ips.ap.gps.5.txt.3600." ]]; then
+    #     input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
+    #     num_frames=7
+    #     width=738
+    #     height=738
+
+    #     block_sizes=(70 125 247)
+    # fi
+    # if [[ ${filename} == "tm.sort_ips.ap.gps.1.sub_CN.txt.3600." ]]; then
+    #     input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
+    #     num_frames=7
+    #     width=410
+    #     height=410
+
+    #     block_sizes=(41 103 205)
+    # fi
+    # if [[ ${filename} == "tm.sort_ips.ap.bgp.8.txt.3600." ]]; then
+    #     input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
+    #     num_frames=7
+    #     width=421
+    #     height=421
+
+    #     block_sizes=(43 106 211)
+    # fi
+    # if [[ ${filename} == "tm.sort_ips.ap.bgp.10.sub_CN.txt.3600." ]]; then
+    #     input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
+    #     num_frames=7
+    #     width=403
+    #     height=403
+
+    #     block_sizes=(41 101 202)
+    # fi
+    # #############
+    if [[ ${filename} == "tm_download.sort_ips.ap.bgp.sub_CN.txt.3600.top400." ]]; then
         input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
         num_frames=8
-        width=400
+        width=217
         height=400
 
-        block_sizes=(40 100 200)
+        block_sizes=(0 1 2 3)
+        block_ws=(22 40 55 110)
+        block_hs=(40 40 100 200)
     fi
-    if [[ ${filename} == "tm.sort_ips.ap.gps.5.txt.3600." ]]; then
-        input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
-        num_frames=8
-        width=738
-        height=738
-
-        block_sizes=(70 125 247)
-    fi
-    if [[ ${filename} == "tm.sort_ips.ap.gps.1.sub_CN.txt.3600." ]]; then
-        input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
-        num_frames=8
-        width=410
-        height=410
-
-        block_sizes=(41 103 205)
-    fi
-    if [[ ${filename} == "tm.sort_ips.ap.bgp.8.txt.3600." ]]; then
-        input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
-        num_frames=8
-        width=421
-        height=421
-
-        block_sizes=(43 106 211)
-    fi
-    if [[ ${filename} == "tm.sort_ips.ap.bgp.10.sub_CN.txt.3600." ]]; then
-        input_dir="\/u\/yichao\/anomaly_compression\/condor_data\/subtask_parse_sjtu_wifi\/tm\/"
-        num_frames=8
-        width=403
-        height=403
-
-        block_sizes=(41 101 202)
-    fi
-    #############
 
     seeds=(1 2 3 4 5 6 7 8 9 10)
     opt_swap_mats=(0 1 3)
-    loss_rates=(0.005 0.01 0.05)
-    opt_deltas=(1 2)
+    loss_rates=(0 0.01 0.05 0.1 0.2 0.3)
+    opt_deltas=(1)
     # opt_f_bs=`seq 18 26`
     opt_f_bs=(16 18 19 21)
 
@@ -204,10 +218,11 @@ for filename in "tm.sort_ips.ap.country.txt.3600."  "tm.sort_ips.ap.bgp.8.txt.36
 
 
                         for block_size in ${block_sizes[@]}; do
-                            echo ${func}.${filename}.${num_frames}.${width}.${height}.${block_size}.${block_size}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}
-                            sed "s/INPUT_DIR/${input_dir}/g; s/FILENAME/${filename}/g;s/NUM_FRAMES/${num_frames}/g;s/BLOCK_HEIGHT/${block_size}/g;s/BLOCK_WIDTH/${block_size}/g;s/WIDTH/${width}/g;s/HEIGHT/${height}/g;s/OPT_DELTA/${opt_delta}/g;s/OPT_FRAMES/${opt_frame}/g;s/OPT_BLOCKS/${opt_block}/g;s/OPT_FRAME_BLOCK/${opt_f_b}/g;s/OPT_SWAP_MAT/${opt_swap_mat}/g;s/SEED/${seed}/g;s/LOSS_RATE/${loss_rate}/g" ${func}.mother.sh > tmp.${func}.${filename}.${num_frames}.${width}.${height}.${block_size}.${block_size}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}.sh
-                            sed "s/XXX/${filename}.${num_frames}.${width}.${height}.${block_size}.${block_size}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}/g" ${func}.mother.condor > tmp.${func}.${filename}.${num_frames}.${width}.${height}.${block_size}.${block_size}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}.condor
-                            condor_submit tmp.${func}.${filename}.${num_frames}.${width}.${height}.${block_size}.${block_size}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}.condor
+
+                            echo ${func}.${filename}.${num_frames}.${width}.${height}.${block_ws[$block_size]}.${block_hs[$block_size]}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}
+                            sed "s/INPUT_DIR/${input_dir}/g; s/FILENAME/${filename}/g;s/NUM_FRAMES/${num_frames}/g;s/BLOCK_HEIGHT/${block_hs[$block_size]}/g;s/BLOCK_WIDTH/${block_ws[$block_size]}/g;s/WIDTH/${width}/g;s/HEIGHT/${height}/g;s/OPT_DELTA/${opt_delta}/g;s/OPT_FRAMES/${opt_frame}/g;s/OPT_BLOCKS/${opt_block}/g;s/OPT_FRAME_BLOCK/${opt_f_b}/g;s/OPT_SWAP_MAT/${opt_swap_mat}/g;s/SEED/${seed}/g;s/LOSS_RATE/${loss_rate}/g" ${func}.mother.sh > tmp.${func}.${filename}.${num_frames}.${width}.${height}.${block_ws[$block_size]}.${block_hs[$block_size]}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}.sh
+                            sed "s/XXX/${filename}.${num_frames}.${width}.${height}.${block_ws[$block_size]}.${block_hs[$block_size]}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}/g" ${func}.mother.condor > tmp.${func}.${filename}.${num_frames}.${width}.${height}.${block_ws[$block_size]}.${block_hs[$block_size]}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}.condor
+                            condor_submit tmp.${func}.${filename}.${num_frames}.${width}.${height}.${block_ws[$block_size]}.${block_hs[$block_size]}.${opt_delta}.${opt_f_b}.${opt_swap_mat}.${loss_rate}.${seed}.condor
                         done
                     done
                 done
