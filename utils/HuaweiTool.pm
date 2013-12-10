3035f1ea9865e15e5082bd16c0ec863a8e9ca492
yichao0319 <yichao0319@gmail.com>
2013-12-10 10:34:16 -0600

    TM of 3G data

---
+++ utils/HuaweiTool.pm
@@ -0,0 +1,67 @@
+
+package HuaweiTool;
+
+use strict;
+use POSIX;
+use List::Util qw(first max maxstr min minstr reduce shuffle sum);
+
+
+sub read_bs_location {
+    my ($fullpath) = @_;
+
+    my $DEBUG0 = 0;
+    my $DEBUG1 = 1;
+
+    my %bs_info = ();
+
+    open FH, "$fullpath" or die $!;
+    while(<FH>) {
+        my ($bs_id, $lng, $lat) = split(/\s+/, $_);
+        $bs_id += 0; $lat += 0; $lng += 0;
+
+        print "bs=$bs_id, lat=$lat, lng=$lng\n" if($DEBUG0);
+
+        $bs_info{$bs_id}{LAT} = $lat;
+        $bs_info{$bs_id}{LNG} = $lng;
+    }
+    close FH;
+
+    return %bs_info;
+}
+
+
+sub read_traffic {
+    my ($fullpath) = @_;
+
+    my $DEBUG0 = 0;
+    my $DEBUG1 = 1;
+
+    my %traffic_info = ();
+    my $num_values;
+
+    open FH, "$fullpath" or die $!;
+    while(<FH>) {
+        my ($cell_id, @val) = split(/\s+/, $_);
+        $cell_id += 0;
+        $num_values = scalar(@val);
+        for my $i (0 .. @val-1) {
+            $val[$i] += 0;
+        }
+
+        print "cell=$cell_id, val=".join(",", @val)."\n" if($DEBUG0);
+
+        @{ $traffic_info{$cell_id}{TRAFFIC} } = @val;
+    }
+    close FH;
+
+    return ($num_values, %traffic_info);
+}
+
+
+sub cell2bs {
+    my ($cell_id) = @_;
+
+    return floor($cell_id / 10);
+}
+
+1;
