f7be749d4bc77085a42a6b0b76f1cc248042715d
yichao0319 <yichao0319@gmail.com>
2014-01-08 17:41:52 -0600

    read cells/BSs/RNCs/BS type/latlng info

--- utils/HuaweiTool.pm
+++ utils/HuaweiTool.pm
@@ -30,6 +30,35 @@ sub read_bs_location {
 }
 
 
+sub read_bs_info {
+    ## <RNC_ID> <BS_ID> <Cell_ID> <Longitude> <Latitude> <BS_Type>
+    my ($fullpath) = @_;
+
+    my $DEBUG0 = 0;
+    my $DEBUG1 = 1;
+
+    my %bs_info = ();
+
+    open FH, "$fullpath" or die $!;
+    while(<FH>) {
+        chomp;
+        my ($rnc_id, $bs_id, $cell_id, $lng, $lat, $bs_type) = split(/\s+/, $_);
+        $rnc_id += 0; $bs_id += 0; $cell_id += 0; $lat += 0; $lng += 0; $bs_type += 0;
+
+        print "rnc=$rnc_id, bs=$bs_id (type=$bs_type), cell=$cell_id, lat=$lat, lng=$lng\n" if($DEBUG0);
+
+        $bs_info{CELL}{$cell_id}{RNC} = $rnc_id;
+        $bs_info{CELL}{$cell_id}{BS} = $bs_id;
+        $bs_info{CELL}{$cell_id}{BS_TYPE} = $bs_type;
+        $bs_info{CELL}{$cell_id}{LAT} = $lat;
+        $bs_info{CELL}{$cell_id}{LNG} = $lng;
+    }
+    close FH;
+
+    return %bs_info;
+}
+
+
 sub read_traffic {
     my ($fullpath) = @_;
 
@@ -39,7 +68,8 @@ sub read_traffic {
     my %traffic_info = ();
     my $num_values;
 
-    open FH, "$fullpath" or die $!;
+    # open FH, "$fullpath" or die $!;
+    open FH, "bzcat $fullpath |" or die $!;
     while(<FH>) {
         my ($cell_id, @val) = split(/\s+/, $_);
         $cell_id += 0;
